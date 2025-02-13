bf3357cd717fb0b741941d48893ceea5d9a43232
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-13 16:18:46 +0530

    middleware learnign is Completed

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+// Middle ware :-
+const reqFilter = require("./middleware");
+
+const route = express.Router();
+
+
+// app.use(reqFilter)
+// Specific Middleware 
+route.use(reqFilter)
+route.get("/users", (req, res) => {
+  res.send("welcome  to users Page");
+});
+
+route.get("/contact", (req, res) => {
+  res.send("Welcome to Cotnact Page")
+})
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page")
+})
+
+app.get("/about", (req, res) => {
+  res.send("welcome  to users Page");
+});
+
+app.use("/" , route)
+
+app.listen(4500, () => {
+  console.log("Server is Started...")
+})
---
+++ middleware.js
@@ -0,0 +1,10 @@
+// middleware
+module.exports = reqFilter = (req, res, next) => {
+  if (!req.query.age) {
+    res.send("Please provide Age");
+  } else if (req.query.age < 18) {
+    res.send("You cannot access this page");
+  } else {
+    next();
+  }
+};
